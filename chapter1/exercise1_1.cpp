c138696c62812683c73a5e1b67d8e83c91249077
PatrickM24 <fanpatrick8@gmail.com>
2025-01-21 06:31:16 +0000

    exercise 1_1

--- chapter1/exercise1_1.cpp
+++ chapter1/exercise1_1.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
 //recursive function to calculate fibonnaci numbers
 
-
+int fib(int n, int one = 0, int two = 1){
+    if (n==0){
+        return two;
+    }
+    else{
+        int helper = two;
+        two = two + one;
+        one = helper;
+        return fib(n-1, one, two);
+    }
+}
