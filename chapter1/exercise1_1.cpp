19148b83cf890c902a19460bdd0b4f68e06b33ed
lighteningAB <124469718+lighteningAB@users.noreply.github.com>
2025-01-21 15:40:30 +0000

    finished fibonacci

--- chapter1/exercise1_1.cpp
+++ chapter1/exercise1_1.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 //recursive function to calculate fibonnaci numbers
 
-int fib(int n, int one = 0, int two = 1){
-    if (n==0){
-        return two;
+int fib(int n, int prev = 0, int curr = 1){
+    if (n<=0){
+        return 0;
+    }
+    else if (n==1){
+        return curr;
     }
     else{
-        int helper = two;
-        two = two + one;
-        one = helper;
-        return fib(n-1, one, two);
+        int helper = curr;
+        curr = prev + curr;
+        prev = helper;
+        return fib(n-1, prev, curr);
     }
+}
+
+int main() {
+    int n;
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+    std::cout << "Fibonacci(" << n << ") = " << fib(n) << std::endl;
+    return 0;
 }
