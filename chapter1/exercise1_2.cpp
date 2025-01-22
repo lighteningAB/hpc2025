f63aa2d56f60fb9091624cff72c86a60579521dc
PatrickFan <124469718+lighteningAB@users.noreply.github.com>
2025-01-22 19:55:29 +0000

    move and make correct and run

--- chapter1/exercise1_2.cpp
+++ chapter1/exercise1_2.cpp
@@ -1,44 +1,46 @@
 #include <cstdlib>
 #include <iostream>
+#include <cmath>
 
 double pi_4_approx(int n)
 {
-    double returnable = 0;
+    double returnable = 0.0;
     for (int i = 1; i < n + 1; i++)
     {
         int k = i - 1;
-        returnable += (-1) ^ k / (2 * k + 1);
+        returnable += pow(-1,k) / (2.0 * k + 1.0);
     }
     return returnable;
 }
 
 double pi_2_approx(int n)
 {
-    double returnable = 1;
+    double returnable = 1.0;
     for (int i = 1; i < n + 1; i++)
     {
-        returnable = returnable * (4 * i ^ 2) / (4 * i ^ 2 - 1);
+        returnable = returnable * (4.0 * pow(i,2)) / (4.0 * pow(i,2) - 1.0);
     }
     return returnable;
 }
 
-void main(){
-    int prev = 100;
-    int curr = 1000;
+int main(){
+    double prev = 100.0;
+    double curr = 1000.0;
     int n = 1;
-    while (!(abs(prev-curr)<0.00001)){
+    while (fabs(prev-curr)>0.00001){
         prev = curr;
         curr = pi_4_approx(n);
-        n++;
+        n+=1;
     }
     std::cout << "The value of pi is " << curr*4 << " using the first approximation, found in "<< n << " iterations" <<std::endl;
-    int prev = 100;
-    int curr = 1000;
-    int n = 1;
-    while (!(abs(prev-curr)<0.00001)){
+    prev = 100;
+    curr = 1000;
+    n = 1;
+    while (fabs(prev-curr)>0.00001){
         prev = curr;
         curr = pi_2_approx(n);
-        n++;
+        n+=1;
     }
     std::cout << "The value of pi is " << curr*2 << " using the second approximation, found in "<< n << " iterations" <<std::endl;
+    return 0;
 }
