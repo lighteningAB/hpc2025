b5539a5e646d33621b85e0c986953ed195a5ab11
PatrickFan <124469718+lighteningAB@users.noreply.github.com>
2025-02-10 22:22:20 +0000

    guhhh

---
+++ chapter4/exercise4_6/exercise4_6.cpp
@@ -0,0 +1,17 @@
+//1 prompt user for grid points, step size, number of time steps
+#include <iostream>
+
+int main(){
+    int gridp;
+    double ss;
+    int ts;
+    std::cout<<"how many grid points to use?: ";
+    std::cin>>gridp;  
+    std::cout<<std::endl;
+    std::cout<<"what stepsize?: ";
+    std::cin>>ss;
+    std::cout<<std::endl;
+    std::cout<<"amount of timesteps?: ";
+    std::cin>>ts;
+    std::cout<<std::endl;
+}
