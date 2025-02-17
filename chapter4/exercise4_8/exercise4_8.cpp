a8cbdd12a3f5e8d6016c24b1f63d3f31bc291a5b
lighteningAB <124469718+lighteningAB@users.noreply.github.com>
2025-02-17 11:08:30 +0000

    Update exercise4_8.cpp

--- chapter4/exercise4_8/exercise4_8.cpp
+++ chapter4/exercise4_8/exercise4_8.cpp
@@ -3,7 +3,7 @@
 #define F77NAME(x) x##_
 
 extern "C"{
-    void F77NAME(dgemm)(char &transa, char &transb)
+    void F77NAME(dgemm)(char &transa, char &transb);
 }
 
 //function has input of two nxn arrays where n is a multiple of 2 and returns the product
